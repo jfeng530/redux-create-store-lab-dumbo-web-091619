5bbd34e1c76c0853d159a5a708b35bab258b3399
Jacky Feng <jackyfeng530@yahoo.com>
2019-12-09 17:06:13 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ function createStore(reducer) {
   let state;
   
   function dispatch(action) {
-    state = reducer(state, action)
+    state = reducer(state, action);
     render();
   }
   
-  function getState(){
-    return state
+  function getState() {
+    return state;
   }
   
   return {
     getState,
     dispatch
-  }
+  };
 }
 
 
